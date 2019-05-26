a45ffede5d05d77357bb4e8f11d4ce6fd05ad722
Oliver Kurz <okurz@suse.de>
2019-05-26 12:15:29 +0200

    Add (temporary?) firefox start helper

---
+++ bin/startfirefox.sh
@@ -0,0 +1,1 @@
+firefox "https://openqa.opensuse.org?group=openSUSE Tumbleweed$|openSUSE Leap [0-9]{2}.?[0-9]*$|openSUSE Leap.*JeOS$|openSUSE Krypton|openQA|GNOME Next&limit_builds=2&time_limit_days=14&&show_tags=1&fullscreen=1#build-results"
